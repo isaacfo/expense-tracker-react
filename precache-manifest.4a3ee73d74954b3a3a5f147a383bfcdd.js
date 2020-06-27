ac238d68772d83f6a5eeb79e9bf3f24353fbce31
isaacfonsecalopez <isaacfo1010@gmail.com>
2020-06-27 01:09:30 -0400

    Updates

---
+++ precache-manifest.4a3ee73d74954b3a3a5f147a383bfcdd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac4ca2a5812fb72fc62a0ea90373a3b4",
+    "url": "/expense-tracker-react/index.html"
+  },
+  {
+    "revision": "454f519cdf876be92558",
+    "url": "/expense-tracker-react/static/css/main.03af9b7a.chunk.css"
+  },
+  {
+    "revision": "a3886eb48eeb822dea54",
+    "url": "/expense-tracker-react/static/js/2.abb637fc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/expense-tracker-react/static/js/2.abb637fc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "454f519cdf876be92558",
+    "url": "/expense-tracker-react/static/js/main.e6946944.chunk.js"
+  },
+  {
+    "revision": "27d5a3916d5c0aa966b2",
+    "url": "/expense-tracker-react/static/js/runtime-main.69aaf8cd.js"
+  }
+]);
